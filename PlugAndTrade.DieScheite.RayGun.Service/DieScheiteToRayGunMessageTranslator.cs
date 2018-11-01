2f564d81a827a54d4a61573b7a44519e81204d9a
Henrik Jönsson <henke@plug-n-trade.com>
2018-11-01 14:29:15 +0100

    Stacktraces

---
+++ PlugAndTrade.DieScheite.RayGun.Service/DieScheiteToRayGunMessageTranslator.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Elmah;
+using Mindscape.Raygun4Net;
+using Newtonsoft.Json;
+using PlugAndTrade.DieScheite.Client.Common;
+
+namespace PlugAndTrade.DieScheite.RayGun.Service
+{
+    public class DieScheiteToRayGunMessageTranslator
+    {
+        public RaygunMessage Translate(LogEntry logEntry)
+        {
+            return new RaygunMessage
+            {
+                Details = new RaygunMessageDetails
+                {
+                    Client = new RaygunClientMessage
+                    {
+                        Name = logEntry.ServiceId,
+                        Version = logEntry.ServiceVersion
+                    },
+                    UserCustomData = GetUserCustomData(logEntry),
+                    Error = GetRaygunError(logEntry),
+                    Request = CreateRaygunRequestMessage(logEntry),
+                    Response = CreateRaygunResponseMessage(logEntry)
+                }
+            };
+        }
+
+        private static RaygunResponseMessage CreateRaygunResponseMessage(LogEntry logEntry)
+        {
+            if (logEntry.Http?.Response == null)
+            {
+                return null;
+            }
+
+            return new RaygunResponseMessage
+            {
+                Content = GetTextContent(logEntry.Http.Response.Body),
+                StatusCode = logEntry.Http.Response.StatusCode ?? 0
+            };
+        }
+
+        private static RaygunRequestMessage CreateRaygunRequestMessage(LogEntry logEntry)
+        {
+            if (logEntry.Http?.Request == null)
+            {
+                return null;
+            }
+
+            var requestDetails = new RaygunRequestMessage
+            {
+                HttpMethod = logEntry.Http.Request.Method,
+                Url = logEntry.Http.Request.Uri,
+                HostName = logEntry.Http.Request.Host,
+                RawData = GetTextContent(logEntry.Http.Request.Body),
+                Headers = logEntry.Http.Request.Headers?.Where(h => !string.IsNullOrEmpty(h.Key)).ToLookup(h => h.Key, h => h.Value).ToDictionary(g => g.Key, g => g.First())
+            };
+
+            // TODO Uri.Query only works for absolute urls, wtf?
+            if (logEntry.Http.Request.Uri.Contains('?'))
+            {
+                var queryString = HttpUtility.ParseQueryString(logEntry.Http.Request.Uri.Split('?').Last());
+                var applicableKeys = queryString.AllKeys.Where(s => !string.IsNullOrEmpty(s));
+                requestDetails.QueryString = applicableKeys.ToDictionary(k => k, k => queryString[k]);
+            }
+
+            return requestDetails;
+        }
+
+        private static RaygunErrorMessage GetRaygunError(LogEntry logEntry)
+        {
+            if (logEntry.Messages.Count == 1)
+            {
+                var message = logEntry.Messages.Single();
+                return CreateRaygunErrorMessage(message);
+            }
+
+            return new RaygunErrorMessage
+            {
+                InnerErrors = logEntry.Messages.Select(CreateRaygunErrorMessage).ToArray(),
+                Message = "Multiple errors"
+            };
+        }
+
+        private static RaygunErrorMessage CreateRaygunErrorMessage(LogEntryMessage message)
+        {
+            return new RaygunErrorMessage
+            {
+                Message = message.Message,
+                StackTrace = CreateStackTrace(message.Stacktrace),
+                Data = GetRaygunErrorMessageData(message)
+            };
+        }
+
+        private static Dictionary<string, string> GetRaygunErrorMessageData(LogEntryMessage message)
+        {
+            var data = new Dictionary<string, string>
+            {
+                {"level", Convert.ToString(message.Level)}
+            };
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                data.Add("attachmentIds", string.Join(",", message.Attachments.Select(a => a.Id)));
+            }
+
+            if (!string.IsNullOrEmpty(message.TraceId))
+            {
+                data.Add("traceId", message.TraceId);
+            }
+
+            return data;
+        }
+
+        private static RaygunErrorStackTraceLineMessage[] CreateStackTrace(string stacktrace)
+        {
+            try
+            {
+                return StackTraceParser.Parse(
+                    stacktrace,
+                    (idx, len, txt) => new { Index = idx, Length = len, Text = txt },
+                    (type, method) => new { Type = type, Method = method },
+                    (type, name) => new { Type = type, Name = name },
+                    (pl, ps) => new { List = pl, Parameters = ps },
+                    (file, line) => new { File = file, Line = line },
+                    (f, tm, p, fl) => new RaygunErrorStackTraceLineMessage
+                    {
+                        FileName = fl.File.Text,
+                        ClassName = tm.Type.Text,
+                        MethodName = tm.Method.Text,
+                        LineNumber = int.TryParse(fl.Line.Text, out var lineNo) ? lineNo : 0
+                    })
+                    .ToArray();
+            }
+            catch
+            {
+                return new RaygunErrorStackTraceLineMessage[0];
+            }
+        }
+
+        private static Dictionary<string, string> GetUserCustomData(LogEntry logEntry)
+        {
+            var customData = new Dictionary<string, string>
+            {
+                { "id", logEntry.Id },
+                { "correlationId", logEntry.CorrelationId },
+                { "duration", Convert.ToString(logEntry.Duration) },
+            };
+
+            if (!string.IsNullOrEmpty(logEntry.ParentId))
+            {
+                customData.Add("parentId", logEntry.ParentId);
+            }
+
+            foreach (var header in logEntry.Headers ?? new List<KeyValuePair<string, object>>())
+            {
+                if (!string.IsNullOrEmpty(header.Key))
+                {
+                    customData.Add(header.Key, JsonConvert.ToString(header.Value));
+                }
+            }
+
+            return customData;
+        }
+
+        private static string GetTextContent(byte[] body)
+        {
+            try
+            {
+                return Encoding.UTF8.GetString(body);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
--- PlugAndTrade.DieScheite.RayGun.Service/Program.cs
+++ PlugAndTrade.DieScheite.RayGun.Service/Program.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Mindscape.Raygun4Net;
 using Newtonsoft.Json;
@@ -23,9 +20,10 @@ namespace PlugAndTrade.DieScheite.RayGun.Service
             var raygunClient = serviceProvider.GetService<RaygunClient>();
 
             var consumer = serviceProvider.GetService<RabbitMQClientFactory>().CreateQueueConsumer(
-		Environment.GetEnvironmentVariable("RABBITMQ_QUEUE_NAME"),
-		1,
- 		m => OnMessage(raygunClient, m));
+                Environment.GetEnvironmentVariable("RABBITMQ_QUEUE_NAME"),
+                1,
+                m => OnMessage(raygunClient, serviceProvider.GetService<DieScheiteToRayGunMessageTranslator>(), m));
+
             consumer.Start();
 
             WaitForExit(() =>
@@ -59,20 +57,21 @@ namespace PlugAndTrade.DieScheite.RayGun.Service
                 Environment.GetEnvironmentVariable("RABBITMQ_CONNECTIONNAME")));
 
             services.AddSingleton(c => new RaygunClient(Environment.GetEnvironmentVariable("RAYGUN_API_KEY")));
+            services.AddSingleton<DieScheiteToRayGunMessageTranslator>();
 
             return services.BuildServiceProvider();
         }
 
-        private static bool OnMessage(RaygunClient raygunClient, Message message)
+        private static bool OnMessage(RaygunClient raygunClient, DieScheiteToRayGunMessageTranslator translator, Message message)
         {
             try
             {
                 var logEntry = ReadLogEntry(message);
-                if (logEntry.Level < (int) LogEntryLevel.Warning)
+                if (logEntry.Level < (int)LogEntryLevel.Warning)
                 {
                     return true;
                 }
-                var raygunMessage = CreateRaygunMessage(logEntry);
+                var raygunMessage = translator.Translate(logEntry);
                 var task = raygunClient.Send(raygunMessage);
                 if (!task.Wait(TimeSpan.FromSeconds(60)))
                 {
@@ -82,147 +81,13 @@ namespace PlugAndTrade.DieScheite.RayGun.Service
 
                 return true;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e);
                 return false;
             }
         }
 
-        private static RaygunMessage CreateRaygunMessage(LogEntry logEntry)
-        {
-            return new RaygunMessage
-            {
-                Details = new RaygunMessageDetails
-                {
-                    Client = new RaygunClientMessage
-                    {
-                        Name = logEntry.ServiceId,
-                        Version = logEntry.ServiceVersion
-                    },
-                    UserCustomData = GetUserCustomData(logEntry),
-                    Error = GetRaygunError(logEntry),
-                    Request = CreateRaygunRequestMessage(logEntry),
-                    Response = CreateRaygunResponseMessage(logEntry)
-                }
-            };
-        }
-
-        private static RaygunResponseMessage CreateRaygunResponseMessage(LogEntry logEntry)
-        {
-            if (logEntry.Http?.Response == null)
-            {
-                return null;
-            }
-
-            return new RaygunResponseMessage
-            {
-                Content = GetTextContent(logEntry.Http.Response.Body),
-                StatusCode = logEntry.Http.Response.StatusCode ?? 0
-            };
-        }
-
-        private static RaygunRequestMessage CreateRaygunRequestMessage(LogEntry logEntry)
-        {
-            if (logEntry.Http?.Request == null)
-            {
-                return null;
-            }
-
-            var requestDetails = new RaygunRequestMessage
-            {
-                HttpMethod = logEntry.Http.Request.Method,
-                Url = logEntry.Http.Request.Uri,
-                HostName = logEntry.Http.Request.Host,
-                RawData = GetTextContent(logEntry.Http.Request.Body),
-                Headers = logEntry.Http.Request.Headers?.Where(h => !string.IsNullOrEmpty(h.Key)).ToLookup(h => h.Key, h => h.Value).ToDictionary(g => g.Key, g => g.First())
-            };
-
-            // TODO Uri.Query only works for absolute urls, wtf?
-            if (logEntry.Http.Request.Uri.Contains('?'))
-            {
-                var queryString = HttpUtility.ParseQueryString(logEntry.Http.Request.Uri.Split('?').Last());
-                var applicableKeys = queryString.AllKeys.Where(s => !string.IsNullOrEmpty(s));
-
-                requestDetails.QueryString = applicableKeys.ToDictionary(k => k, k => queryString[k]);
-            }
-
-            return requestDetails;
-        }
-
-        private static RaygunErrorMessage GetRaygunError(LogEntry logEntry)
-        {
-            if (logEntry.Messages.Count == 1)
-            {
-                var message = logEntry.Messages.Single();
-                return CreateRaygunErrorMessage(message);
-            }
-
-            return new RaygunErrorMessage
-            {
-                InnerErrors = logEntry.Messages.Select(CreateRaygunErrorMessage).ToArray(),
-                Message = "Multiple errors"
-            };
-        }
-
-        private static RaygunErrorMessage CreateRaygunErrorMessage(LogEntryMessage message)
-        {
-            return new RaygunErrorMessage
-            {
-                Message = message.Message,
-                StackTrace = CreateStackTrace(message.Stacktrace),
-                Data = GetRaygunErrorMessageData(message)
-            };
-        }
-
-        private static Dictionary<string, string> GetRaygunErrorMessageData(LogEntryMessage message)
-        {
-            var data = new Dictionary<string, string>
-            {
-                {"level", Convert.ToString(message.Level)}
-            };
-
-            if (message.Attachments != null && message.Attachments.Any())
-            {
-                data.Add("attachmentIds", string.Join(",", message.Attachments.Select(a => a.Id)));
-            }
-
-            if (!string.IsNullOrEmpty(message.TraceId))
-            {
-                data.Add("traceId", message.TraceId);
-            }
-
-            return data;
-        }
-
-        private static RaygunErrorStackTraceLineMessage[] CreateStackTrace(string stacktrace)
-        {
-            return new RaygunErrorStackTraceLineMessage[0];
-        }
-
-        private static Dictionary<string, string> GetUserCustomData(LogEntry logEntry)
-        {
-            var customData = new Dictionary<string, string>
-            {
-                { "id", logEntry.Id },
-                { "correlationId", logEntry.CorrelationId },
-                { "duration", Convert.ToString(logEntry.Duration) },
-            };
-
-            if (!string.IsNullOrEmpty(logEntry.ParentId))
-            {
-                customData.Add("parentId", logEntry.ParentId);
-            }
-
-            foreach (var header in logEntry.Headers ?? new List<KeyValuePair<string, object>>())
-            {
-                if (!string.IsNullOrEmpty(header.Key)) {
-                    customData.Add(header.Key, JsonConvert.ToString(header.Value));
-                }
-            }
-
-            return customData;
-        }
 
         private static LogEntry ReadLogEntry(Message arg)
         {
@@ -234,25 +99,15 @@ namespace PlugAndTrade.DieScheite.RayGun.Service
             }
         }
 
-        private static Stream DecodePayload(Message arg) {
+        private static Stream DecodePayload(Message arg)
+        {
             var stream = new MemoryStream(arg.Data);
-            if (arg.GetHeaderValue("Content-Encoding") == "gzip") {
+            if (string.Equals(arg.GetHeaderValue("Content-Encoding"), "gzip", StringComparison.InvariantCultureIgnoreCase))
+            {
                 return new GZipStream(stream, CompressionMode.Decompress);
             }
 
             return stream;
-        }
-
-        private static string GetTextContent(byte[] body)
-        {
-            try
-            {
-                return Encoding.UTF8.GetString(body);
-            }
-            catch
-            {
-                return string.Empty;
-            }
         }
     }
 }
